b155efbed8a435e2b68a4197e12fc0712f0ec0ea
Ahmad Ansori Palembani <palembani@gmail.com>
2024-07-02 12:53:25 +0700

    refactor: Group them up
    Also "fix" the modal's typing issue

--- bot/battle/battle_bot.py
+++ bot/battle/battle_bot.py
@@ -2,31 +2,26 @@ from discord import app_commands
 import discord
 
 def setup_battle_commands(tree: app_commands.CommandTree):
-    
-    @tree.command(name="battle_help", description="Show Battle Tutorial!")
+    group = app_commands.Group(name="battle")
+
+    @group.command(name="help", description="Show Battle Tutorial!")
     async def battle_help(interaction: discord.Interaction):
-        
         await interaction.response.send_message(f"Battle Help Triggered!",ephemeral=True)
 
-    @tree.command(name="battle_action", description="Do An Action!")
-    async def battle_help(interaction: discord.Interaction):
-        
+    @group.command(name="action", description="Do An Action!")
+    async def battle_action(interaction: discord.Interaction):
         await interaction.response.send_message(f"Battle Action Triggered!",ephemeral=True)
 
-    @tree.command(name="battle_check", description="Check Your Stats and Other Useful Stuff In Battle!!")
-    async def battle_help(interaction: discord.Interaction):
-        
+    @group.command(name="check", description="Check Your Stats and Other Useful Stuff In Battle!!")
+    async def battle_check(interaction: discord.Interaction):
         await interaction.response.send_message(f"Battle Check Triggered!",ephemeral=True)
 
-    @tree.command(name="battle_use", description="Check Your Stats and Other Useful Stuff In Battle!!")
+    @group.command(name="use", description="Check Your Stats and Other Useful Stuff In Battle!!")
     async def battle_use(interaction: discord.Interaction):
-        
         await interaction.response.send_message(f"Battle Use Item Triggered!",ephemeral=True)
 
-    @tree.command(name="battle_flee", description="Run from the current battle!")
+    @group.command(name="flee", description="Run from the current battle!")
     async def battle_flee(interaction: discord.Interaction):
-        
         await interaction.response.send_message(f"Battle Flee Triggered!",ephemeral=True)
-    
-    
-   
+
+    tree.add_command(group)
--- bot/dungeon/dungeon_bot.py
+++ bot/dungeon/dungeon_bot.py
@@ -3,27 +3,29 @@ import discord
 
 #The INITS
 def setup_dungeon_commands(tree: app_commands.CommandTree):
-    
-    @tree.command(name="dungeon_help", description="Show Battle Tutorial!")
+    group = app_commands.Group(name="dungeon")
+
+    @group.command(name="help", description="Show Battle Tutorial!")
     async def dungeon_help(interaction: discord.Interaction):
         dungeon_help = show_dungeon_help()
         await interaction.response.send_message(dungeon_help, ephemeral=True)
 
-    @tree.command(name="dungeon_create", description="Create a New Dungeon!")
+    @group.command(name="create", description="Create a New Dungeon!")
     async def dungeon_create(interaction: discord.Interaction):
         result = dungeon_creation_check(interaction)
         await interaction.response.send_message(result,ephemeral=True)
 
-    @tree.command(name="dungeon_choice", description="Make a choice during encounter!")
+    @group.command(name="choice", description="Make a choice during encounter!")
     async def dungeon_choice(interaction: discord.Interaction):
         result = pick_choices(interaction)
         await interaction.response.send_message(result,ephemeral=True)
 
-    @tree.command(name="dungeon_abandon", description="Abandon The Dungeon (WILL DESTROY THE DUNGEON)")
+    @group.command(name="abandon", description="Abandon The Dungeon (WILL DESTROY THE DUNGEON)")
     async def dungeon_abandon(interaction: discord.Interaction):
         result = pick_abandon(interaction)
         await interaction.response.send_message(result,ephemeral=True)
-    
+
+    tree.add_command(group)
 
 
 
@@ -33,17 +35,17 @@ def show_dungeon_help():
     # This just pulls up the help menu
     return "Help Here~"
 
-def dungeon_creation_check(data):
+def dungeon_creation_check(interaction: discord.Interaction):
     # check if all is set (enough materials etc)
     # queue create dungeon
     return "Dungeon Creation in Progress~"
 
-def pick_choices():
+def pick_choices(interaction: discord.Interaction):
     # check if input is correct
     # queue "Consequences Of Thy Action" (prompt to llm)
     return "Choices Evaluation in Progress"
 
-def pick_abandon():
+def pick_abandon(interaction: discord.Interaction):
     # evaluate user input
     # update user data
     # await destroy dungeon
